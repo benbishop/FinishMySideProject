7506c3d4303a0749ab767e03fd84bfb49a1da25c
Ben Bishop <ben@freckleinteractive.net>
2012-05-17 08:35:42 -0500

    Initial app generation.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FinishMySideProject::Application.config.secret_token = '0655e7d47e1305ccae33c3442822c49500137148ff54068db287804d77cafa20c91286bc6b3cd5f01d729a5e566fad7d50a0151a7d202e9e764643f2147ab992'
