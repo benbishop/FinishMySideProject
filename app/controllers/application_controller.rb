dde8faa059255002b32a21758df098cc6c7361a0
Ben Bishop <ben@freckleinteractive.net>
2012-05-17 08:57:53 -0500

    Restored methods.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  def index
+    render :template => 'application/index'
+  end
 
+
+  def about
+    render :template => 'application/about'
+  end
+
+
+  def privacy_policy
+    render :template => 'application/privacy_policy'
+  end
+
+  def terms_of_use
+    render :template => 'application/terms_of_use'
+  end
 end
